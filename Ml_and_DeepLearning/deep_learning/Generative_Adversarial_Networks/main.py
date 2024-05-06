b6ec3db5949bffb303cca392c46abbaeda8fc7af
Zephyrus <zephyrus1175@yahoo.com>
2024-05-07 00:15:00 +0300

    GAN's

---
+++ Ml_and_DeepLearning/deep_learning/Generative_Adversarial_Networks/main.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+import torch.utils.data
+import torch.utils.data
+
+from labml_helpers.module import Module
+
+
+class DiscriminatorLogitsLoss(Module):
+    def __init__(self, smoothing: float = 0.2):
+        super().__init__()
+        self.loss_true = nn.BCEWithLogitsLoss()
+        self.loss_false = nn.BCEWithLogitsLoss()
+        self.smoothing = smoothing
+
+        self.register_buffer('labels_true', _create_labels(256, 1.0 - smoothing, 1.0), False)
+        self.register_buffer('labels_false', _create_labels(256, 0.0 - smoothing), False)
+
+    def forward(self, logits_true: torch.Tensor, logits_false: torch.Tensor):
+        if len(logits_true) > len(self.labels_true):
+            self.register_buffer('labels_true', _create_labels(len(logits_true), 1.0 - self.smoothing,
+                                                               1.0, logits_true.device), False)
+        if len(logits_false) > len(self.label_false):
+            self.register_buffer('labels_false', _create_labels(len(logits_false), 0.0, self.smoothing,
+                                                                logits_false.device), False)
+
+        return (self.loss_true(logits_true, self.labels_true[:len(logits_true)]),
+                self.loss_false(logits_false, self.labels_false[:len(logits_false)]))
+
+
+class GeneratorLogitsLoss(Module):
+    def __init__(self, smoothing: float = 0.2):
+        super().__init__()
+        self.loss_true = nn.BCEWithLogitsLoss()
+        self.smoothing = smoothing
+
+        self.register_buffer('fake_labels', _create_labels(256, 1.0 - smoothing, 1.0), False)
+
+    def forward(self, logits: torch.Tensor):
+        if len(logits) > len(self.fake_labels):
+            self.register_buffer('fake_labels', _create_labels(len(logits), 1.0 - self.smoothing, 1.0,
+                                                               logits.device), False)
+
+        return self.loss_true(logits, self.fake_labels[:len(logits)])
+
+
+def _create_labels(n: int, r1: float, r2: float, device: torch.device = None):
+    return torch.empty(n, 1, requires_grad=False, device=device).uniform_(r1, r2)
